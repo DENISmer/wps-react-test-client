a1c7d8eb522b1eeae73e1123d6e8702064ebe716
Денис <rediskink@inbox.ru>
2023-07-21 15:43:49 +0700

    - axios request with parameters  - converting xml response to js  - showing js response

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {domain} from "./config/config";
+import {xml2js, xml2json} from "xml-js";
 
 function App() {
+    const [url,setUrl] = useState();
+    const [result,setResult] = useState([]);
+    let res;
+    let arrayOfId = [];
+    const [error,setError] = useState();
+    const [service,setService] = useState('WPS');
+    const [version,setVersion] = useState('1.0.0');
+    const [requestParam,setRequestParam] = useState('GetCapabilities');
+    const [id,setId] = useState()
+
+    const request = () => {
+    console.log(url)
+    axios.get(`${url}`,{
+        headers:{
+        "Content-type": "text/html",
+        },
+        params:{
+            'service': service,
+            'version': version,
+            'request': requestParam,
+        }
+    })
+        // .elements[0].elements[0].elements[0].text
+        .then((response) => {
+                res = xml2js(response.data).elements[0].elements[3].elements
+            simpleNamingOfProcessors(res)
+            console.log(xml2js(response.data).elements[0].elements[3].elements)
+        })
+        .catch((err) =>{
+            alert(err.message)
+        })
+    }
+    const simpleNamingOfProcessors = (inputArray) => {
+        arrayOfId = [];
+        inputArray.map((item, index)=>(
+            arrayOfId.push(item.elements[0].elements[0].text)
+        ))
+        setResult(arrayOfId)
+    }
+
+    useEffect(()=>{
+        axios.get(`${url}`,{
+            headers:{
+                "Content-type": "text/html",
+            },
+            params:{
+                'service': service,
+                'version': version,
+                'request': 'DescribeProcess',
+                'identifier': id,
+            }
+        })
+            .then((response) => {
+                console.log(xml2json(response.data))
+            })
+            .catch((e) => {
+                console.log(e.message)
+            })
+    },[id])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+            <div>
+                <select style={{height: '40px',fontSize: '20px',fontFamily: "Roboto, sans-serif"}}  onClick={(e) => setUrl(e.target.value)}>{
+                    domain.map((item,index) =>(
+                        <option value={item.name} onClick={(e) => setUrl(item.name)}>{item.name}</option>
+                    ))
+                }</select>
+
+                <select style={{height: '40px',fontSize: '20px',fontFamily: "Roboto, sans-serif"}} onClick={(e) => setId(e.target.value)}>
+                    {
+                        result.map((item,index) =>(
+                            <option value={item} onClick={(e) => {
+                                setId(e.target.value);
+
+                            }}>{item}</option>
+                        ))
+                    }
+                </select>
+                {/*<input list={"browsers"} type={"text"}*/}
+                {/*        placeholder={'Введите название и выберите регион'}*/}
+                {/*        // value={url}*/}
+                {/*        style={{width: '400px'}}*/}
+                {/*        onChange={(e) => {*/}
+                {/*            // setUrl(e.target.value)*/}
+                {/*        }}/>*/}
+
+                {/*<datalist id="browsers">*/}
+                {/*    {domain.map((item,index)=>(<option key={index} value={item.name}>{item.name}</option>)*/}
+                {/*    )}*/}
+                {/*</datalist>*/}
+            </div>
+
+            <div>{error ? error : 'no error'}</div>
+            <button onClick={() =>request()}>TEST</button>
+
     </div>
   );
 }
---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+export const domain = [
+    {name: `http://wps.ict.nsc.ru:8080/wps/WebProcessingService`},
+    {name: `http://wps.esemc.nsc.ru:8080/wps/WebProcessingService`},
+]
+
